9dde50370c094815b8467d4511bcd11fcd0c0e7c
Miguel <mglgnzlz13@gmail.com>
2021-05-30 12:54:16 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace ContactTracingForm_MiguelGonzalez
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name_Label = new System.Windows.Forms.Label();
+            this.Name_Display = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name_Label
+            // 
+            this.Name_Label.AutoSize = true;
+            this.Name_Label.Location = new System.Drawing.Point(13, 84);
+            this.Name_Label.Name = "Name_Label";
+            this.Name_Label.Size = new System.Drawing.Size(42, 15);
+            this.Name_Label.TabIndex = 0;
+            this.Name_Label.Text = "Name:";
+            // 
+            // Name_Display
+            // 
+            this.Name_Display.AutoSize = true;
+            this.Name_Display.Location = new System.Drawing.Point(61, 84);
+            this.Name_Display.Name = "Name_Display";
+            this.Name_Display.Size = new System.Drawing.Size(12, 15);
+            this.Name_Display.TabIndex = 1;
+            this.Name_Display.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Name_Display);
+            this.Controls.Add(this.Name_Label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name_Label;
+        private System.Windows.Forms.Label Name_Display;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
